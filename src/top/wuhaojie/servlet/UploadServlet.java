db7db09bb9436d7c854490e57cd0b08a3bb8a608
wuhaojie <w19961009@126.com>
2016-04-20 21:35:03 +0800

    add file upload.

--- src/top/wuhaojie/servlet/UploadServlet.java
+++ src/top/wuhaojie/servlet/UploadServlet.java
@@ -1,11 +1,21 @@
 package top.wuhaojie.servlet;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.ProgressListener;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * Author: wuhaojie
@@ -20,6 +30,71 @@ public class UploadServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DiskFileItemFactory factory = new DiskFileItemFactory();
 
+        ServletFileUpload servletFileUpload = new ServletFileUpload(factory);
+
+        servletFileUpload.setProgressListener(new ProgressListener() {
+            Long beginTime = System.currentTimeMillis();
+
+            public void update(long bytesRead, long contentLength, int items) {
+                BigDecimal br = new BigDecimal(bytesRead).divide(
+                        new BigDecimal(1024), 2, BigDecimal.ROUND_HALF_UP);
+                BigDecimal cl = new BigDecimal(contentLength).divide(
+                        new BigDecimal(1024), 2, BigDecimal.ROUND_HALF_UP);
+                System.out.print("当前读取的是第" + items + "个上传项,总大小" + cl
+                        + "KB,已经读取" + br + "KB");
+                // 剩余字节数
+                BigDecimal ll = cl.subtract(br);
+                System.out.print("剩余" + ll + "KB");
+                // 上传百分比
+                BigDecimal per = br.multiply(new BigDecimal(100)).divide(cl, 2,
+                        BigDecimal.ROUND_HALF_UP);
+                System.out.print("已经完成" + per + "%");
+                // 上传用时
+                Long nowTime = System.currentTimeMillis();
+                Long useTime = (nowTime - beginTime) / 1000;
+                System.out.print("已经用时" + useTime + "秒");
+                // 上传速度
+                BigDecimal speed = new BigDecimal(0);
+                if (useTime != 0) {
+                    speed = br.divide(new BigDecimal(useTime), 2,
+                            BigDecimal.ROUND_HALF_UP);
+                }
+                System.out.print("上传速度为" + speed + "KB/S");
+                // 大致剩余时间
+                BigDecimal leftTime = new BigDecimal(0);
+                if (!speed.equals(new BigDecimal(0))) {
+                    leftTime = ll.divide(speed, 0, BigDecimal.ROUND_HALF_UP);
+                }
+                System.out.print("大致剩余时间为" + leftTime + "秒");
+
+                System.out.println();
+            }
+
+        });
+
+        try {
+            List<FileItem> list = servletFileUpload.parseRequest(request);
+            for (FileItem item : list) {
+                if (!item.isFormField()) {
+                    InputStream is = item.getInputStream();
+                    FileOutputStream fos = new FileOutputStream(new File(
+                            "d://test.avi"));
+                    byte[] buffer = new byte[1024];
+                    int len;
+                    while ((len = is.read(buffer)) != -1) {
+                        fos.write(buffer, 0, len);
+                    }
+                    System.out.println("写入文件完成！");
+                    is.close();
+                    fos.close();
+                }
+                item.delete();
+                System.out.println("文件上传成功");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -3,14 +3,13 @@
   User: wuhaojie
   Date: 2016/4/20
   Time: 19:13
-  To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+    <title>欢迎</title>
+</head>
+<body>
+<h1>Hello World!</h1>
+</body>
 </html>
