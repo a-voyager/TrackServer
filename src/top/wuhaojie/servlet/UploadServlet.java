a819fd7127c25d70da6dd66f6e42b3dcc610aba0
wuhaojie <w19961009@126.com>
2016-04-20 20:54:49 +0800

    Initial commit.

---
+++ src/top/wuhaojie/servlet/UploadServlet.java
@@ -0,0 +1,25 @@
+package top.wuhaojie.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Author: wuhaojie
+ * E-mail: w19961009@126.com
+ * Date: 2016/4/20 20:52
+ * Version: 1.0
+ */
+@WebServlet(name = "UploadServlet")
+public class UploadServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
